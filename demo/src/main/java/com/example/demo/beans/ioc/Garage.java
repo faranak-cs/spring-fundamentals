99f5d192df8eba2f293a79680c644e8cea322ecc
faran khan <fakhan@beyond.com>
2024-07-01 15:05:24 +0100

    added comments

---
+++ demo/src/main/java/com/example/demo/beans/ioc/Garage.java
@@ -0,0 +1,25 @@
+package com.example.demo.beans.ioc;
+
+import com.example.demo.beans.di.Vehicle;
+import com.example.demo.beans.di.impl.Car;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Garage {
+
+    //
+    // INJECTOR CLASS
+    // 1. INJECTOR USES CLIENT CONSTRUCTOR TO PASS DEPENDENCY (CONSTRUCTOR)
+    // 2. INJECTOR USES PUBLIC PROPERTY TO PASS DEPENDENCY (SETTER)
+    // 3. CLIENT IMPLEMENTS AN INTERFACE METHOD TO RECEIVE DEPENDENCY (INTERFACE)
+    //
+
+    // INSTANTIATE OBJECT OF VEHICLE INTERFACE
+    @Bean
+    Vehicle v(){
+        // USING CONSTRUCTOR
+        return new Car();
+    }
+
+}
